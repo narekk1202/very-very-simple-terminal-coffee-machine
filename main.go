43db85c0570c2688d989269733cb046dd78ee233
narekk1202 <narekdumamaliiian@gmail.com>
2023-11-16 14:30:32 +0400

    Loop added

--- main.go
+++ main.go
@@ -21,36 +21,40 @@ var coffeeOptions = []coffes{
 }
 
 func main() {
-	var selectedCoffee int
-	var sum int
-	var coffee coffes
-	fmt.Println("Choose your coffee!")
-	fmt.Println("========================")
-	printCoffees()
+	for {
+		var selectedCoffee int
+		var sum int
+		var coffee coffes
+		fmt.Println("Choose your coffee!")
+		fmt.Println("========================")
+		printCoffees()
 
-	fmt.Print("Enter your choice: ")
-	fmt.Scan(&selectedCoffee)
-	screen.Clear()
-	screen.MoveTopLeft()
-	coffee = coffeeOptions[selectedCoffee-1]
-
-	if len(coffee.name) > 0 {
-		fmt.Print("Enter the sum: ")
-		fmt.Scan(&sum)
-		if sum == coffee.price {
-			fmt.Println("Your order is", coffee.name)
-			askForSugar()
-		} else if sum > coffee.price {
-			fmt.Printf("Here is your change $%d\n", sum-coffee.price)
-			fmt.Println("Your order is", coffee.name)
-			askForSugar()
-		} else {
-			fmt.Println("Not enough!")
+		fmt.Print("Enter your choice: ")
+		fmt.Scan(&selectedCoffee)
+		clearTerminal()
+		if selectedCoffee > 0 && selectedCoffee <= len(coffeeOptions) {
+			coffee = coffeeOptions[selectedCoffee-1]
 		}
-	} else {
-		fmt.Println("Invalid choice")
-	}
 
+		if len(coffee.name) > 0 {
+			fmt.Print("Enter the sum: ")
+			fmt.Scan(&sum)
+			if sum == coffee.price {
+				fmt.Println("Your order is", coffee.name)
+				askForSugar()
+			} else if sum > coffee.price {
+				fmt.Printf("Here is your change $%d\n", sum-coffee.price)
+				fmt.Println("Your order is", coffee.name)
+				askForSugar()
+			} else {
+				fmt.Println("Not enough!")
+			}
+		} else {
+			fmt.Println("Invalid choice!")
+			time.Sleep(1 * time.Second)
+			clearTerminal()
+		}
+	}
 }
 
 func printCoffees() {
@@ -65,21 +69,28 @@ func askForSugar() {
 
 	fmt.Print("Do you want sugar? 1. Yes 2. No: ")
 	fmt.Scan(&acceptSugar)
-	screen.Clear()
-	screen.MoveTopLeft()
+	clearTerminal()
 
 	if acceptSugar == 1 {
 		fmt.Print("How many spoons of sugar you want: ")
 		fmt.Scan(&spoons)
-		screen.Clear()
-		screen.MoveTopLeft()
+		clearTerminal()
 	}
 
-	if spoons != 0 {
+	if spoons != 0 && spoons <= 5 {
 		fmt.Println("Wait for your coffee!")
 		time.Sleep(time.Duration(spoons) * time.Second)
 		fmt.Println("Your coffee is ready!")
-	} else {
+		time.Sleep(3 * time.Second)
+		clearTerminal()
+	} else if acceptSugar == 2 {
 		fmt.Println("Here is your coffee, enjoy!")
+		time.Sleep(3 * time.Second)
+		clearTerminal()
 	}
+}
+
+func clearTerminal() {
+	screen.Clear()
+	screen.MoveTopLeft()
 }
